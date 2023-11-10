42c1a20707527c2b36ddd9bd752dff1cbc7b597a
RhonFarber <122325331+RhonFarber@users.noreply.github.com>
2023-11-10 23:52:21 +0000

    commit

--- answers.md
+++ answers.md
@@ -1,8 +1,28 @@
 # CMPS 2200 Assignment 4
 ## Answers
 
-**Name:**_________________________
+**Name:** Rhon Farber
 
 
 Place all written answers from `assignment-04.md` here for easier grading.
 
+1a)
+The greedy algorithm for producing as few coins as possible that sum to 
+N would be continuously choosing the highest denomination coin that fits without exceeding the target dollar amount. The algorithm repeats this process until the total value matches the specified dollar amount.
+
+1b)
+The greedy choice is choosing the largest denomination that does not exceed the remaining amount. The optimal substructure properties are proven as follows: given a target N, at each iteration, the greedy choice is made and since the denomations are in base 2, the coin chosen is 2^k. Then the subproblem becomes N - 2^k. This substructure continues as N - 2^k becomes the new N and 2^k is the next largest denomiation that doesn't exceed N. No combination of coins less than 2^k can sum up to 2^k without using more coins at each step, so the solution is optimal.
+
+1c)
+The work is W(n) = O(log(n)) 
+The span is S(n) = O(log(n)) 
+This is because the greedy algorithm does log_2(n) over and over again until n = 0, which is not a parallelizable process. 
+
+2a)
+A simple counterexample that shows that the greedy algorithm does not produce the fewest number of coins in Fortuito could be if there are denominations of 1, 3, and 4. The target N is 6. The greedy algorithm would choose 4, and then 1, and then 1 to reach 4 + 1 + 1 = 6. This is not optimal because there is a better solution of choosing 3 and then choosing 3 which is 3 + 3 = 6. This solution uses 2 coins rather than the 3 coins from the greedy solution. 
+
+2b)
+In Fortuito, the optimal substructure property is as follows. Assuming you have an optimal solution for making change for N dollars using the fewest coins. The first coin of denomination D is subtracted from the target N. Now the subproblem is N - D. If that subproblem is not optimal then you replace that part of the solution with a better solution for N - D. 
+
+2c)
+Using dynamic programming and a bottom-up approach. You can build a 2d array of denominations and their powers. The work is equal to all the nodes of DAG, or W(n) = O(n * k) where n is the length of the set of denominations and k is the highest powers. The span is the longest path in DAG, which is the table so it is S(n) = O(n * k).
--- main.py
+++ main.py
@@ -3,36 +3,69 @@ from collections import Counter
 
 ####### Problem 3 #######
 
-test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
-alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
+test_cases = [('book', 'back'), ('kookaburra', 'kookybird'),
+              ('elephant', 'relevant'), ('AAAGAATTCA', 'AAATCA')]
+alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'),
+              ('relev--ant', '-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
+
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+  # TO DO - modify to account for insertions, deletions and substitutions
+  if (S == ""):
+    return (len(T))
+  elif (T == ""):
+    return (len(S))
+  else:
+    if (S[0] == T[0]):
+      return (MED(S[1:], T[1:]))
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+      return (1 + min(MED(S, T[1:]), MED(S[1:], T)))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+  # top-down memoization
+  key = (S, T)
+  if key not in MED:
+    if (S == ""):
+      MED[key] = len(T)
+    elif (T == ""):
+      MED[key] = len(S)
+    else:
+      if (S[0] == T[0]):
+        MED[key] = fast_MED(S[1:], T[1:], MED)
+      else:
+        MED[key] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED))
+  return MED[key]
+
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+  key = (S, T)
+  if key not in MED:
+    if S == "":
+      MED[key] = (len(T), "-" * len(T), T)
+    elif T == "":
+      MED[key] = (len(S), S, "-" * len(S))
+    else:
+      if S[0] == T[0]:
+        dist, align_S, align_T = fast_align_MED(S[1:], T[1:], MED)
+        MED[key] = (dist, S[0] + align_S, T[0] + align_T)
+      else:
+        dist1, align_S1, align_T1 = fast_align_MED(S, T[1:], MED)
+        dist2, align_S2, align_T2 = fast_align_MED(S[1:], T, MED)
+        if dist1 < dist2:
+          MED[key] = (dist1 + 1, S[0] + align_S1, "-" + align_T1)
+        else:
+          MED[key] = (dist2 + 1, "-" + align_S2, T[0] + align_T2)
+  return MED[key]
+
 
 def test_MED():
-    for S, T in test_cases:
-        assert fast_MED(S, T) == MED(S, T)
-                                 
+  for S, T in test_cases:
+    assert fast_MED(S, T) == MED(S, T)
+
+
 def test_align():
-    for i in range(len(test_cases)):
-        S, T = test_cases[i]
-        align_S, align_T = fast_align_MED(S, T)
-        assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+  for i in range(len(test_cases)):
+    S, T = test_cases[i]
+    align_S, align_T = fast_align_MED(S, T)
+    assert (align_S == alignments[i][0] and align_T == alignments[i][1])
